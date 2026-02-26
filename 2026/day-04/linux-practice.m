570f487493372546d473a30e9ab877151613c6b8
Rushikesh Nalawade <rnalwade0@gmail.com>
2026-02-26 18:51:57 +0530

    Linux Fundamentals with real command

---
+++ 2026/day-04/linux-practice.m
@@ -0,0 +1,67 @@
+*ps aux*
+What To Observe:
+
+USER
+
+PID (Process ID)
+
+CPU usage
+
+MEMORY usage
+
+COMMAND
+
+I observed that system processes like systemd, sshd, and bash are running.
+
+*Command 2 *
+
+ top
+
+ What To Observe:
+
+CPU usage 
+
+Memory usage
+
+Load average
+
+Running processes count
+
+
+*Command 3 *
+
+ps aux | frep ssh
+
+SSH process is running with PID
+
+4) Small Troubleshooting Flow (Real DevOps Scenario)
+Scenario:
+
+Website not accessible.
+
+Step 1: Check if Service is Running
+Step 2: Check Port Listening
+Step 3: Check Logs
+Write:
+
+Found error: ______
+Fixed by: ______
+
+
+hence day 4 tasks completed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
